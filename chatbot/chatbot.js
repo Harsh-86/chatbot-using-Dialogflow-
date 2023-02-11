17e764f25add0b90aade8427bafe7753ea4f19e9
Harsh-86 <harshpipalia21@gmail.com>
2023-02-11 10:17:28 +0530

    updated

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -1,36 +1,36 @@
-const dialogflow = require('dialogflow');
-const config = require('../config/devkey');
+const dialogflow = require("dialogflow");
+const config = require("../config/devkey");
 
 const projectId = config.googleProjectId;
 const sessionId = config.dialogFlowSessionID;
 
 const credentials = {
-    client_email: config.googleClientEmail,
-    private_key: config.googlePrivateKey
-}
-const sessionClient = new dialogflow.SessionsClient({projectId, credentials});
+  client_email: config.googleClientEmail,
+  private_key: config.googlePrivateKey,
+};
+const sessionClient = new dialogflow.SessionsClient({ projectId, credentials });
 
-const textQuery = async(userText, userId)=>{
-    const sessionPath = sessionClient.sessionPath(projectId, sessionId+userId);
-    const request = {
-        session: sessionPath,
-        queryInput: {
-            text: {
-                text: userText,
-                languageCode: config.dialogFlowSessionLanguageCode
-            }
-        }
-    }
+const textQuery = async (userText, userId) => {
+  const sessionPath = sessionClient.sessionPath(projectId, sessionId + userId);
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        text: userText,
+        languageCode: config.dialogFlowSessionLanguageCode,
+      },
+    },
+  };
 
-    try{
-        const respone= await sessionClient.detectIntent(request)
-        return respone[0].queryResult
-    }catch(err){
-        console.log(err)
-        return err
-    }
-}
+  try {
+    const respone = await sessionClient.detectIntent(request);
+    return respone[0].queryResult;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+};
 
 module.exports = {
-    textQuery
-}
+  textQuery,
+};
--- server.js
+++ server.js
@@ -1,141 +1,161 @@
-const express = require('express');
-const port = process.env.PORT || 3000;
-const bodyParser = require('body-parser');
-const cors = require('cors')
-const diff = require('dialogflow-fulfillment');
-var mysql = require('mysql');
+const express = require("express");
+const port = process.env.PORT || 3001;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const diff = require("dialogflow-fulfillment");
+var mysql = require("mysql");
 var app = express();
-app.use(cors())
-
+app.use(cors());
 
 var con = mysql.createConnection({
-    host: "localhost",  //when using with other device use ngrok.......
-    user: "root",
-    password: "",
-    database: "SDB"
+  host: "localhost", //when using with other device use ngrok.......
+  user: "root",
+  password: "",
+  database: "SDB",
 });
 
 con.connect();
 
-app.post('/', express.json(), (req, res) => {
-    const agent = new diff.WebhookClient({
-        request: req,
-        response: res
-    });
+app.post("/", express.json(), (req, res) => {
+  const agent = new diff.WebhookClient({
+    request: req,
+    response: res,
+  });
 
-    // function demo(agent){
+  // function demo(agent){
 
-    //     agent.add("Sending response from Webhook server");
-    // }
+  //     agent.add("Sending response from Webhook server");
+  // }
 
-
-    async function resultq(agent) {
-        let cgpa;
-        var sql = "SELECT CGPA FROM studentdb WHERE Enrollment_No = " + agent.parameters.number[0];
-        if (agent.parameters.number[0] > 92000103000 && agent.parameters.number[0] < 92000103021) {
-            con.query(sql, function (err, result) {
-                if (err) throw err;
-                console.log(result[0].CGPA);
-                cgpa = result[0].CGPA;
-                return result[0].CGPA;
-            });
-            // wait for the query to finish
-            await new Promise(resolve => setTimeout(resolve, 1000));
-            agent.add("Your CGPA is " + cgpa);
-        }
-        else {
-            agent.add("Please enter a valid Enrollment Number!");
-        }
+  async function resultq(agent) {
+    let cgpa;
+    var sql =
+      "SELECT CGPA FROM studentdb WHERE Enrollment_No = " +
+      agent.parameters.number[0];
+    console.log(sql);
+    if (
+      agent.parameters.number[0] > 92000103000 &&
+      agent.parameters.number[0] < 92000103021
+    ) {
+      con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log(result[0].CGPA);
+        cgpa = result[0].CGPA;
+        return result[0].CGPA;
+      });
+      // wait for the query to finish
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      agent.add("Your CGPA is " + cgpa);
+    } else {
+      agent.add("Please enter a valid Enrollment Number!");
     }
+  }
 
-
-    async function fee(agent) {
-        var fee_status;
-        var resp = '';
-        var sql1 = "SELECT Fees FROM studentdb WHERE Enrollment_No = " + agent.parameters.number[0];
-        if (agent.parameters.number[0] > 92000103000 && agent.parameters.number[0] < 92000103021) {
-            con.query(sql1, function (err, result1) {
-                if (err) throw err;
-                console.log(result1[0].Fees);
-                fee_status = result1[0].Fees;
-                return result1[0].Fees;
-            });
-            // wait for the query to finish
-            await new Promise(resolve => setTimeout(resolve, 1000));
-            resp = "Your fee is " + fee_status;
-        }
-        else {
-            resp = "Please enter a valid Enrollment Number!";
-        }
-        agent.add(resp);
+  async function fee(agent) {
+    var fee_status;
+    var resp = "";
+    var sql1 =
+      "SELECT Fees FROM studentdb WHERE Enrollment_No = " +
+      agent.parameters.number[0];
+    if (
+      agent.parameters.number[0] > 92000103000 &&
+      agent.parameters.number[0] < 92000103021
+    ) {
+      con.query(sql1, function (err, result1) {
+        if (err) throw err;
+        console.log(result1[0].Fees);
+        fee_status = result1[0].Fees;
+        return result1[0].Fees;
+      });
+      // wait for the query to finish
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      resp = "Your fee is " + fee_status;
+    } else {
+      resp = "Please enter a valid Enrollment Number!";
     }
+    agent.add(resp);
+  }
 
-    async function attend(agent) {
-        var attendance;
-        var resp = '';
-        var sql2 = "SELECT Attendance FROM studentdb WHERE Enrollment_No = " + agent.parameters.number[0];
-        if (agent.parameters.number[0] > 92000103000 && agent.parameters.number[0] < 92000103021) {
-            con.query(sql2, function (err, result2) {
-                if (err) throw err;
-                console.log(result2[0].Attendance);
-                attendance = result2[0].Attendance;
-                return result2[0].Attendance;
-            });
-            // wait for the query to finish
-            await new Promise(resolve => setTimeout(resolve, 1000));
-            resp = "Your Attendance is " + attendance+"%";
-        }
-        else {
-            resp = "Please enter a valid Enrollment Number!";
-        }
-        agent.add(resp);
+  async function attend(agent) {
+    var attendance;
+    var resp = "";
+    var sql2 =
+      "SELECT Attendance FROM studentdb WHERE Enrollment_No = " +
+      agent.parameters.number[0];
+    if (
+      agent.parameters.number[0] > 92000103000 &&
+      agent.parameters.number[0] < 92000103021
+    ) {
+      con.query(sql2, function (err, result2) {
+        if (err) throw err;
+        console.log(result2[0].Attendance);
+        attendance = result2[0].Attendance;
+        return result2[0].Attendance;
+      });
+      // wait for the query to finish
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      resp = "Your Attendance is " + attendance + "%";
+    } else {
+      resp = "Please enter a valid Enrollment Number!";
     }
+    agent.add(resp);
+  }
 
-    async function placement(agent) {
-        var cgpa;
-        var branch;
-        var resp = '';
-        var sql2 = "SELECT CGPA,Branch FROM studentdb WHERE Enrollment_No = " + agent.parameters.number[0];
-        if (agent.parameters.number[0] > 92000103000 && agent.parameters.number[0] < 92000103021) {
-            con.query(sql2, function (err, result2) {
-                if (err) throw err;
-                console.log('aaaaa',result2[0].cgpa);
-                cgpa = result2[0].CGPA;
-                branch = result2[0].Branch;
-            });
-            // wait for the query to finish
-            await new Promise(resolve => setTimeout(resolve, 1000));
-            console.log("aaa",cgpa);
-            // round off cgpa to 0 decimal places
-            var cgpa1 = cgpa;
-            cgpa = Math.round(cgpa);
-            const sql3 = "SELECT Companies FROM placement WHERE cgpa > " + cgpa + " AND branch = '" + branch + "'";
-            // SELECT Companies from placement WHERE CGPA > 8 AND Branch = 'LAW'
-            var companies = [];
-            con.query(sql3, function (err, result2) {
-                if (err) throw err;
-                console.log(result2[0].Companies);
-                companies = result2;
-                // branch = result2[0].Branch;
-            });
-            // wait for the query to finish
-            await new Promise(resolve => setTimeout(resolve, 1000));
-            resp = "Your eligible companies are " + (companies[0].Companies + ', as your CGPA is ' + cgpa1);
-        }
-        else {
-            resp = "Please enter a valid Enrollment Number!";
-        }
-        agent.add(resp);
+  async function placement(agent) {
+    var cgpa;
+    var branch;
+    var resp = "";
+    var sql2 =
+      "SELECT CGPA,Branch FROM studentdb WHERE Enrollment_No = " +
+      agent.parameters.number[0];
+    if (
+      agent.parameters.number[0] > 92000103000 &&
+      agent.parameters.number[0] < 92000103021
+    ) {
+      con.query(sql2, function (err, result2) {
+        if (err) throw err;
+        console.log("aaaaa", result2[0].cgpa);
+        cgpa = result2[0].CGPA;
+        branch = result2[0].Branch;
+      });
+      // wait for the query to finish
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      console.log("aaa", cgpa);
+      // round off cgpa to 0 decimal places
+      var cgpa1 = cgpa;
+      cgpa = Math.round(cgpa);
+      const sql3 =
+        "SELECT Companies FROM placement WHERE cgpa > " +
+        cgpa +
+        " AND branch = '" +
+        branch +
+        "'";
+      // SELECT Companies from placement WHERE CGPA > 8 AND Branch = 'LAW'
+      var companies = [];
+      con.query(sql3, function (err, result2) {
+        if (err) throw err;
+        console.log(result2[0].Companies);
+        companies = result2;
+        // branch = result2[0].Branch;
+      });
+      // wait for the query to finish
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      resp =
+        "Your eligible companies are " +
+        (companies[0].Companies + ", as your CGPA is " + cgpa1);
+    } else {
+      resp = "Please enter a valid Enrollment Number!";
     }
+    agent.add(resp);
+  }
 
-    var intentMap = new Map();
-    intentMap.set('Result', resultq)
-    intentMap.set('fees', fee)
-    intentMap.set('attendance', attend)
-    intentMap.set('Placement', placement)
-    agent.handleRequest(intentMap);
+  var intentMap = new Map();
+  intentMap.set("Result");
+  intentMap.set("fees", fee);
+  intentMap.set("attendance", attend);
+  intentMap.set("Placement", placement);
+  agent.handleRequest(intentMap);
 });
-
 
 // con.connect(function(err) {
 //   if (err) throw err;
@@ -150,29 +170,30 @@ app.post('/', express.json(), (req, res) => {
 //     });
 //   });
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.get("/df/server", (req, res) => {
+  res.send("Hi, from Server");
+});
 
-app.get('/df/server', (req, res) => {
-    res.send("Hi, from Server")
-})
-
-require('./routes/df-routes')(app)
+require("./routes/df-routes")(app);
 
 app.listen(port, () => {
-    console.log("server is running");
-})
+  console.log("server is running");
+});
 
-app.get('/user/:id', (req, res) => {
-    const id = req.params.id;
-    con.connect(function (err) {
+app.get("/user/:id", (req, res) => {
+  const id = req.params.id;
+  con.connect(function (err) {
+    if (err) throw err;
+    con.query(
+      `SELECT * FROM studentdb WHERE Class = '${id}'`,
+      function (err, result, fields) {
         if (err) throw err;
-        con.query(`SELECT * FROM studentdb WHERE Class = '${id}'`, function (err, result, fields) {
-            if (err) throw err;
-            console.log(result);
-            res.send(result)
-        });
-    });
-
-})
+        console.log(result);
+        res.send(result);
+      }
+    );
+  });
+});
